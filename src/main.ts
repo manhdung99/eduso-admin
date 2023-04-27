4269128eadac3a9f3fb2a8d37b58fa5c8497adc1
manhdung99 <manhdung99it@gmail.com>
2023-04-27 09:21:41 +0700

    remove vue-resize package

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,10 @@ import Antd from "ant-design-vue";
 import { createPinia } from "pinia";
 import "ant-design-vue/dist/antd.css";
 import "./assets/css/main.css";
-import VueColumnsResizable from "vue-columns-resizable";
 const pinia = createPinia();
 const app = createApp(App);
 
 app.use(Antd);
 app.use(router);
 app.use(pinia);
-app.use(VueColumnsResizable);
 app.mount("#app");
